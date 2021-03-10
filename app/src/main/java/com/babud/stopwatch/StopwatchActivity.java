f25582b4a9a3a14ec6a0cb89a0a9592a1650954f
AdamBabud <adam.babud@gmail.com>
2021-03-10 20:43:16 +0100

    onStop added

--- app/src/main/java/com/babud/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/babud/stopwatch/StopwatchActivity.java
@@ -10,6 +10,7 @@ public class StopwatchActivity extends Activity {
 
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +19,31 @@ public class StopwatchActivity extends Activity {
         if (savedInstanceState != null){
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
         runTimer();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (wasRunning){
+            running = true;
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt("seconds", seconds);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
     }
 
     public void onClickStart(View view){
